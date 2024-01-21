18326955c0188334bf765bad3db34eba123131ee
Iriam Gonzalez <igboffill@digitalprojex.com>
2024-01-21 10:00:08 -0500

    lesson 2

--- myApp.js
+++ myApp.js
@@ -2,8 +2,12 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI);
 
-
-let Person;
+const personSchema = new mongoose.Schema({
+  name: {type:String, required:true},
+  age: Number,
+  favoriteFoods: String
+})
+let Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
